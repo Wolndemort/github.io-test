3fd91f6bde668be442b82a5ee0f6816daec47c19
wolndemort <kid.cudi.1995@mail.ru>
2026-04-15 14:53:49 +0300

    speedy

--- seed.py
+++ seed.py
@@ -1,23 +1,23 @@
 import asyncio
 import os
-
-from database.db import AsyncSessionLocal
-from database.db import Club # проверь путь к импорту
+from datetime import datetime, timedelta
+from database.db import AsyncSessionLocal, Club # Проверь, что async_session_maker так называется
 
 
 async def seed_first_club():
+    # Используем фабрику сессий, которая у тебя в db.py
     async with AsyncSessionLocal() as session:
-        # Добавляем твой основной токен как первый клуб
         admin_club = Club(
             name="Главная панель",
             bot_token=os.getenv("BOT_TOKEN"),
-            is_active=True,
+            # ЗАМЕНИЛИ is_active на новую колонку и дали подписку на 100 лет
+            subscription_expire_at=datetime.now() + timedelta(days=36500),
             club_settings={},
             owner_id=1271717628
         )
         session.add(admin_club)
         await session.commit()
-        print("✅ Мастер-бот добавлен в базу! Теперь запускай main.py")
+        print("✅ Мастер-бот добавлен! Подписка активна до 2126 года.")
 
 if __name__ == "__main__":
     asyncio.run(seed_first_club())
