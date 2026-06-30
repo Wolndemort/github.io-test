b6227356f95c0d02d3f6852887f5c26d1c34e060
Wolndemort <151738991+Wolndemort@users.noreply.github.com>
2026-06-30 23:24:30 +0300

    Update buttons.py

--- handlers/buttons.py
+++ handlers/buttons.py
@@ -17,7 +17,7 @@ def get_profile_keyboard(club_settings: dict, is_authorized: bool = False):
 
     # 1. Всегда доступные кнопки (или базовые)
     builder.row(types.InlineKeyboardButton(text='➕ Добавить атлета', callback_data='add_athlete'))
-    builder.row(types.InlineKeyboardButton(text='📊 Статус абонемента', callback_data='detailed_status_info'))
+    builder.row(types.InlineKeyboardButton(text='🔍 Подробно об атлетах', callback_data='detailed_status_info'))
 
     # 2. ДИНАМИЧЕСКИЕ КНОПКИ (Показываем только если включено в админке)
 
