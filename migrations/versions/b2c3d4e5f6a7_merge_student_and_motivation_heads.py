1e898996293f999ef11cac2d528e8a80e044010b
wolndemort <kid.cudi.1995@mail.ru>
2026-09-15 15:19:55 +0300

    Merge Alembic migration heads

---
+++ migrations/versions/b2c3d4e5f6a7_merge_student_and_motivation_heads.py
@@ -0,0 +1,13 @@
+"""merge student registration and motivation migration heads"""
+from alembic import op
+
+revision = "b2c3d4e5f6a7"
+down_revision = ("a1b2c3d4e5f6", "e4f5a6b7c8d9")
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    pass
+
+def downgrade():
+    pass
